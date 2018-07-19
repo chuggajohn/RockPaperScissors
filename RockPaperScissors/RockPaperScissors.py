3dc5e93b38df774d6d5fd605c7df7e2d7df5dc55
cmRyanLau <lau.c.m.ryan@gmail.com>
2018-07-19 15:02:19 -0230

    updated RPS python

--- RockPaperScissors/RockPaperScissors.py
+++ RockPaperScissors/RockPaperScissors.py
@@ -2,41 +2,25 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-#cap = cv2.VideoCapture(0)
-
-#while(True):
-#    # Capture frame-by-frame
-#    ret, frame = cap.read()
-
-#    # Our operations on the frame come here
-#    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-#    # Display the resulting frame
-#    cv2.imshow('frame',gray)
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#        break
-
-
-## When everything done, release the capture
-#cap.release()
-#cv2.destroyAllWindows()
 
 cap = cv2.VideoCapture(0)
 
-fgbg = cv2.createBackgroundSubtractorMOG()
+#cv2.xfeatures2d.SIFT_create()
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+ret1, frame1 = cap.read();
 
 while(1):
     ret, frame = cap.read()
 
-    fgmask = fgbg.apply(frame)
+    fgmask = fgbg.apply(frame, frame1)
 
     cv2.imshow('frame',fgmask)
-    k = cv2.waitKey(30) & 0xff
+    k = cv2.waitKey(100) & 0xff
     if k == 27:
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
 
 
