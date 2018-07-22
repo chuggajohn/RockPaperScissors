3ed65f83ba947bc0c0827c2f8ec9029baad92c09
chuggajohn <jab550@mun.ca>
2018-07-22 15:34:51 -0230

    added split imaging

--- RockPaperScissors/RockPaperScissors.py
+++ RockPaperScissors/RockPaperScissors.py
@@ -13,7 +13,10 @@ k = cv2.waitKey(1)
 while k%256 != 27:
     k = cv2.waitKey(1)
     ret, frame = cam.read()
-    cv2.imshow("test", frame)
+    frame = np.hsplit(frame,2)
+    line = np.full((frame[0].shape[0],4,3),128)
+    completeframe = np.hstack((frame[0],line,frame[1]))
+    cv2.imshow('test' , np.array(completeframe, dtype = np.uint8 ) )
     
     
 cv2.destroyAllWindows()
@@ -27,7 +30,10 @@ while True:
     ret, frame = cam.read()
     fgmask2 = fgbg2.apply(frame)
     what = cv2.bitwise_and(frame, frame, mask=fgmask2)
-    cv2.imshow("masked applied",what)
+    hilt = np.hsplit(what,2)
+    hilt[0]
+    cv2.imshow("masked applied",hilt[0])
+    cv2.imshow("masked applied2",hilt[1])
     fgbg2 = cv2.createBackgroundSubtractorMOG2(0,50)
     fgmask2 = fgbg2.apply(oldframe) 
     k = cv2.waitKey(1)
